5ae1b9998ef00330af0b5a394c244610df3af36b
Swarsele <leon.schwarzaeugl@gmail.com>
2022-08-01 01:57:10 +0200

    update

--- joukowski.py
+++ joukowski.py
@@ -11,14 +11,14 @@ def karman_trefftz_transform(zeta, n):
 
 
 def make_karman_trefftz(mux=0.2, muy=0.1, n=1.9, N=100):
-    t0 = -mux + muy * 1j  # center
+    center = -mux + muy * 1j  # center
     R = np.sqrt((1 + mux) ** 2 + muy ** 2)  # radius
 
     theta = np.linspace(0, 2 * np.pi, N)
-    Xc = np.real(t0) + R * np.cos(theta)
-    Yc = np.imag(t0) + R * np.sin(theta)
-    tx = np.real(t0)
-    ty = np.imag(t0)
+    Xc = np.real(center) + R * np.cos(theta)
+    Yc = np.imag(center) + R * np.sin(theta)
+    tx = np.real(center)
+    ty = np.imag(center)
 
     p = karman_trefftz_transform(Xc + Yc * 1j, n)
     Xp, Yp = np.real(p), np.imag(p)
@@ -46,29 +46,29 @@ def joukowski_ca(a, R, muy, t):
     return (8 * R * np.pi * np.sin(a + np.arcsin(muy/R)))/t
 
 def make_joukowski_all(mux=0.2, muy=0.1, N=100):
-    t0 = -mux + muy * 1j  # center
+    center = -mux + muy * 1j  # center
     R =  np.sqrt((1 + mux) ** 2 + muy ** 2)  # radius
     Rci = np.sqrt((-1 + -mux) ** 2 + muy ** 2)# radius
 
     theta = np.linspace(0, 2 * np.pi, N)
-    Xc = np.real(t0) + R * np.cos(theta)
-    Yc = np.imag(t0) + R * np.sin(theta)
-    tx = np.real(t0)
-    ty = np.imag(t0)
+    Xc = np.real(center) + R * np.cos(theta)
+    Yc = np.imag(center) + R * np.sin(theta)
+    tx = np.real(center)
+    ty = np.imag(center)
 
     p = joukowski_transfrom(Xc + Yc * 1j)
     Xp, Yp = np.real(p), np.imag(p)
     return Xp, Yp, Xc, Yc, tx, ty, muy, mux, R, Rci
 
 def make_karman_trefftz_all(mux=0.2, muy=0.1, n=1.9, N=100):
-    t0 = (-mux + muy * 1j)  # center
+    center = (-mux + muy * 1j)  # center
     R = np.sqrt((1 + mux) ** 2 + muy ** 2)  # radius
 
     theta = np.linspace(0, 2 * np.pi, N)
-    Xc = np.real(t0) + R * np.cos(theta)
-    Yc = np.imag(t0) + R * np.sin(theta)
-    tx = np.real(t0)
-    ty = np.imag(t0)
+    Xc = np.real(center) + R * np.cos(theta)
+    Yc = np.imag(center) + R * np.sin(theta)
+    tx = np.real(center)
+    ty = np.imag(center)
 
     p = karman_trefftz_transform(Xc + Yc * 1j, n)
     Xp, Yp = np.real(p), np.imag(p)
